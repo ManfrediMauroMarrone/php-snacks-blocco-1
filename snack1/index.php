b14b3566c7e42eb181bb930e12922b7340c5b523
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-12-15 17:11:21 +0100

    modifiche css e dati snack 1

--- snack1/index.php
+++ snack1/index.php
@@ -3,20 +3,20 @@
 $matches = [
   // creo i sotto array
   [
-    'casa' => 'pippo',
-    'ospite' => 'paperino',
-    'punti_casa' => 20,
-    'punti_ospite' => 30
+    'casa' => 'Virtus Bologna',
+    'ospite' => 'Pall. Cantù',
+    'punti_casa' => 83,
+    'punti_ospite' => 47
   ],
   [
-    'casa' => 'topolino',
-    'ospite' => 'pluto',
-    'punti_casa' => 35,
-    'punti_ospite' => 15
+    'casa' => 'Reggiana',
+    'ospite' => 'Milano 1958',
+    'punti_casa' => 67,
+    'punti_ospite' => 56
   ],
   [
-    'casa' => 'minnie',
-    'ospite' => 'archimede',
+    'casa' => 'Dinamo Sassari',
+    'ospite' => 'Athletic Genova',
     'punti_casa' => 54,
     'punti_ospite' => 87
   ]
@@ -31,16 +31,22 @@ $matches = [
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <p>
-      <?php
-      // ciclo all'interno dell'array e per ogni elemento stampo le chiavi
-      foreach ($matches as &$value) {
-        echo 'squadra di casa:' .$value['casa'] . ' - ' . 'squadra ospite:' . $value['ospite'] . ' ' . '|' . ' ' . 'punti squadra di casa:' .$value['punti_casa'] . ' - ' . 'punti squadra ospite:' .$value['punti_ospite'] .'<br>';
-      };
+    <h1>Ultime partite giocate</h1>
 
-      ?>
 
-    </p>
+    <div class="">
+      <p>
+        <?php
+        // ciclo all'interno dell'array e per ogni elemento stampo le chiavi
+        foreach ($matches as &$value) {
+          echo 'squadra di casa:' .$value['casa'] . ' - ' . 'squadra ospite:' . $value['ospite'] . ' ' . '|' . ' ' . 'punti squadra di casa:' .$value['punti_casa'] . ' - ' . 'punti squadra ospite:' .$value['punti_ospite'] .'<br>';
+        };
+
+        ?>
+
+      </p>
+
+    </div>
 
   </body>
 </html>
